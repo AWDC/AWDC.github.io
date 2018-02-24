f970d71983692f1337957fc4510f92b0823dd1e1
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2018-02-24 20:06:34 +0530

    edited gallery section

--- gallerySection/js/main.js
+++ gallerySection/js/main.js
@@ -85,9 +85,6 @@
 		}
 
 	};
-
-	
-
 	
 	$(function(){
 		magnifPopup();
@@ -96,5 +93,14 @@
 		animateBoxWayPoint();
 	});
 
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
 
 }());
